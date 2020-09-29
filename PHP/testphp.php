793e10c1248f8732369d7ddb8bbef0aee3194e46
Peeraphat <peraphat_love@hotmail.com>
2020-09-29 09:53:57 +0700

    TEST PHP

---
+++ PHP/testphp.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+</head>
+
+<body>
+  
+  <?php
+  
+    $message = "สวัสดี <br>";
+    echo $message;
+    $myName = 'Peeraphat';
+    $age = 20;
+
+  ?>
+
+ชื่อ : <?=$myName?><br>
+อายุ : <?=$age?> 
+</body>
+</html>
