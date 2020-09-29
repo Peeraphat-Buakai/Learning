31a8f641439077962116acba2350bd873dddd77b
Peeraphat <peraphat_love@hotmail.com>
2020-09-29 10:00:30 +0700

    new

--- PHP/testphp.php
+++ PHP/testphp.php
@@ -15,5 +15,15 @@
 
 ชื่อ : <?=$myName?><br>
 อายุ : <?=$age?> 
+
+<?php
+
+$person = array("fullname" =>"John", "height"=>"173","age"=>"43");
+
+foreach ($person as $key => $value) {
+  echo $key . " = " . $value . "<br>" ;
+}
+
+?>
 </body>
 </html>
